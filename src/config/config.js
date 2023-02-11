0aa5da9bd4b30d368bb46d4a639ee5b691ed25c2
techover <techover@techovers-MBP.lan>
2023-02-11 20:07:13 +0100

    Spotify-new updated

--- src/config/config.js
+++ src/config/config.js
@@ -1,30 +1,30 @@
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-export const clientId = '8297cad7fab544b789a9da9acc340cea';
-export const liveURL = 'https://windahls-spotify.herokuapp.com/';
-export const devURL = 'http://localhost:3000/';
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const clientId = "8297cad7fab544b789a9da9acc340cea";
+export const liveURL = "https://filips-spotify-clone.vercel.app/";
+export const devURL = "http://localhost:3000/";
 export const redirectURL = liveURL;
 export const scopes = [
-	'ugc-image-upload',
-	'user-modify-playback-state',
-	'user-read-playback-state',
-	'user-read-currently-playing',
-	'user-follow-modify',
-	'user-follow-read',
-	'user-read-recently-played',
-	'user-read-playback-position',
-	'user-top-read',
-	'playlist-read-collaborative',
-	'playlist-modify-public',
-	'playlist-read-private',
-	'playlist-modify-private',
-	'app-remote-control',
-	'streaming',
-	'user-read-email',
-	'user-read-private',
-	'user-library-modify',
-	'user-library-read'
+  "ugc-image-upload",
+  "user-modify-playback-state",
+  "user-read-playback-state",
+  "user-read-currently-playing",
+  "user-follow-modify",
+  "user-follow-read",
+  "user-read-recently-played",
+  "user-read-playback-position",
+  "user-top-read",
+  "playlist-read-collaborative",
+  "playlist-modify-public",
+  "playlist-read-private",
+  "playlist-modify-private",
+  "app-remote-control",
+  "streaming",
+  "user-read-email",
+  "user-read-private",
+  "user-library-modify",
+  "user-library-read",
 ];
 
 export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURL}&scope=${scopes.join(
-	'%20'
+  "%20"
 )}&response_type=token&show_dialog=true`;
