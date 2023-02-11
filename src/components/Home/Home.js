0bb92bfb6be00bcffe6c3419db1ad3c3e2f0a382
techover <techover@techovers-MBP.lan>
2023-02-11 20:55:14 +0100

    updated home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,33 +1,49 @@
-import React from 'react';
-import { Box, Button } from '@mui/material';
+import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { textAlign } from "@mui/system";
+import { shadows } from "@mui/system";
 
 const Home = () => {
-	return (
-		<Box
-			sx={{
-				bgcolor: 'background.paper',
-				flex: 1,
-				display: 'flex',
-				justifyContent: 'center',
-				alignItems: 'center',
-				flexDirection: 'column',
-				gap: 5
-			}}
-		>
-			<img
-				src="/img/hero.jpg"
-				style={{ maxHeight: '50%', maxWidth: '50%', borderRadius: '15px' }}
-				alt="Techover"
-			/>
-			<Button
-				size="large"
-				variant="contained"
-				onClick={() => (window.location.href = 'https://www.linkedin.com/in/filip-windahl-98930317a/')}
-			>
-				Kontakta mig!
-			</Button>
-		</Box>
-	);
+  return (
+    <Box
+      sx={{
+        bgcolor: "background.paper",
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: 5,
+      }}
+    >
+      <Typography
+        sx={{
+          color: "text.primary",
+          textAlign: "center",
+          maxWidth: "1200px",
+          fontSize: "62px",
+          textShadow: "3px 3px 5px black",
+        }}
+      >
+        Welcome to my Spotify clone
+      </Typography>
+      <img
+        src="/img/hero.jpg"
+        style={{ maxWidth: "50%", borderRadius: "15px" }}
+        alt="Techover"
+      />
+      <Button
+        size="large"
+        variant="contained"
+        onClick={() =>
+          (window.location.href =
+            "https://www.linkedin.com/in/filip-windahl-98930317a/")
+        }
+      >
+        Kontakta mig!
+      </Button>
+    </Box>
+  );
 };
 
 export default Home;
--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 export const clientId = "8297cad7fab544b789a9da9acc340cea";
-export const liveURL = "https://filips-spotify-clone.vercel.app/";
+// export const liveURL = "https://filips-spotify-clone.vercel.app/";
+export const liveURL = "http://localhost:3000/";
 export const devURL = "http://localhost:3000/";
 export const redirectURL = liveURL;
 export const scopes = [
